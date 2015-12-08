86bead1132d76e39465f7b0612414b1ade0d4b6f
Ethan Chen <intervigil@gmail.com>
2015-12-07 22:26:38 -0800

    Telephony: Update getVoiceCallForwardingFlag return type
    * This returns -1, 0, or 1 now, adjust accordingly.
    
    Change-Id: Ic85e85fb76fbcc7745ec66c8319d5c3ee8076734

--- src/java/com/android/internal/telephony/gsm/GSMPhone.java
+++ src/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -332,7 +332,7 @@ public class GSMPhone extends PhoneBase {
         boolean cf = false;
         IccRecords r = mIccRecords.get();
         if (r != null && r.isCallForwardStatusStored()) {
-            cf = r.getVoiceCallForwardingFlag();
+            cf = r.getVoiceCallForwardingFlag() == IccRecords.CALL_FORWARDING_STATUS_ENABLED;
         } else {
             cf = getCallForwardingPreference();
         }
