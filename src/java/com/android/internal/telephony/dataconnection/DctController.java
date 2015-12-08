7e34e8e23d949d6a7265bc6813dc63b1a40d677c
B--B <dark.schneider.ds@gmail.com>
2015-12-08 16:50:55 +0100

    FIx a mismerge after android-6.0.1_r3

--- src/java/com/android/internal/telephony/dataconnection/DctController.java
+++ src/java/com/android/internal/telephony/dataconnection/DctController.java
@@ -514,19 +514,7 @@ public class DctController extends Handler {
             String specifier = requestInfo.request.networkCapabilities
                 .getNetworkSpecifier();
             if (specifier == null || specifier.equals("")) {
-                if (requestInfo.executed) {
-                    String apn = apnForNetworkRequest(requestInfo.request);
-                    logd("[setDataSubId] subId =" + dataSubId);
-                    requestInfo.log(
-                            "DctController.onSettingsChange releasing request");
-                    for (int i = 0; i < mPhoneNum; i++) {
-                        PhoneBase phoneBase =
-                            (PhoneBase)mPhones[i].getActivePhone();
-                        DcTrackerBase dcTracker = phoneBase.mDcTracker;
-                        dcTracker.decApnRefCount(apn, requestInfo.getLog());
-                        requestInfo.executed = false;
-                    }
-                }
+                onReleaseRequest(requestInfo);
             }
         }
     }
